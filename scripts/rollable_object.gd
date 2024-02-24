1f5f452654ab59362cfb86dc72b36744d80d393e
Ronan Banton <crealol2@outlook.com>
2024-02-24 00:42:57 -0500

    Objects now only stick to katamari core

--- scripts/rollable_object.gd
+++ scripts/rollable_object.gd
@@ -3,9 +3,10 @@ extends AnimatableBody3D
 
 
 
-func _on_katamari_entered(body):
+func _on_katamari_entered(_rid, body, shape, _locshape):
 	print(body)
-	if body.name == "KatamariBody":
+	print(shape)
+	if body.name == "KatamariBody" and shape == 0:
 		$RollableObjectCollision.queue_free()
 		$OnKatamariCollisionShape.reparent(body, true)
 		$RollableObjectMesh.reparent(body.get_node("KatamariMeshPivot"))
