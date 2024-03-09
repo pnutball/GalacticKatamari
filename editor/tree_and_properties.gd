d5527a92284b4492f5e700e0cf64932e61b69ffb
Ronan Banton <crealol2@outlook.com>
2024-03-08 22:49:26 -0500

    Added a tiny bit of property tree functionality

--- editor/tree_and_properties.gd
+++ editor/tree_and_properties.gd
@@ -1,7 +1,17 @@
 extends VSplitContainer
 
+@onready var LevelTreeRoot:TreeItem = %LevelTree.create_item()
+
+const Vector3Property:PackedScene = preload("res://editor/property_vector3.tscn")
+const NumberProperty:PackedScene = preload("res://editor/property_number.tscn")
+const StringProperty:PackedScene = preload("res://editor/property_string.tscn")
+
 func _ready():
-	var root = %LevelTree.create_item()
-	root.set_text(0, "New File")
-	root.set_icon(0, load("res://editor/icons/document.png"))
+	LevelTreeRoot.set_text(0, "New File")
+	LevelTreeRoot.set_icon(0, load("res://editor/icons/document.png"))
 	
+
+
+func _on_properties_list_changed():
+	if %PropertiesPanel:
+		$PropertiesScroll/PropertiesMargin/NoneSelectedLabel.visible = %PropertiesPanel.get_child_count() == 0
