fb0dfb9df27d64c84b6bbdd32db4a7966a26151e
Ronan Banton <crealol2@outlook.com>
2024-03-19 02:44:51 -0400

    Added area loading to stage loader

--- scripts/stage_loader.gd
+++ scripts/stage_loader.gd
@@ -5,9 +5,13 @@ signal stage_instantiated
 
 var objectList:Dictionary = preload("res://data/objects.json").data
 var currentStage:Dictionary
+var currentMode:String
+var currentArea:int = 0
+var stageRoot:Node3D
+var currentKatamari
 
-var RollableObject3D:PackedScene = preload("res://scenes/game/object/rollable_object_3d.tscn")
-var KatamariControllable:PackedScene = preload("res://scenes/game/object/katamari_controllable.tscn")
+const RollableObject3D:PackedScene = preload("res://scenes/game/object/rollable_object_3d.tscn")
+const KatamariControllable:PackedScene = preload("res://scenes/game/object/katamari_controllable.tscn")
 
 func _init():
 	process_mode = Node.PROCESS_MODE_ALWAYS
@@ -59,18 +63,45 @@ func getModes():
 ## mode: The level's mode.
 ## parent: The node that the stage root node is parented to.
 func instantiateStage(mode:String, parent:Node = get_tree().get_root()):
-	var stageRoot = Node3D.new()
+	currentMode = mode
+	stageRoot = Node3D.new()
 	stageRoot.name = "KatamariStageRoot"
+	
+	currentKatamari = KatamariControllable.instantiate()
+	currentKatamari.Size = currentStage.modes.get(currentMode).katamari.size
+	currentKatamari.Speed = currentStage.modes.get(currentMode).katamari.speed
+	currentKatamari.CanDash = currentStage.modes.get(currentMode).katamari.can_dash
+	currentKatamari.CanQuickTurn = currentStage.modes.get(currentMode).katamari.can_turn
+	currentKatamari.CameraRotation = currentStage.modes.get(currentMode).katamari.cam_rotation
+	currentKatamari.CameraZones = currentStage.modes.get(currentMode).cam_zones
+	
+	await instantiateArea(0)
+	
+	stageRoot.add_child(currentKatamari)
+	
+	# INSTANTIATE TIMER HERE
+	
+	parent.add_child(stageRoot)
+	stage_instantiated.emit()
 
-	for scene in currentStage.modes.get(mode).map_zones[0].static:
+func instantiateArea(area:int = currentArea + 1):
+	currentArea = area
+	if not (stageRoot.get_node_or_null("static") == null):
+		stageRoot.static.queue_free()
+	
+	var currentStatics := Node3D.new()
+	currentStatics.name = "static"
+	stageRoot.add_child(currentStatics)
+	
+	for scene in currentStage.modes.get(currentMode).map_zones[currentArea].static:
 		ResourceLoader.load_threaded_request(scene)
 		while ResourceLoader.load_threaded_get_status(scene) == ResourceLoader.THREAD_LOAD_IN_PROGRESS:
 			await get_tree().create_timer(0.1).timeout
 		var instantiated:Node = ResourceLoader.load_threaded_get(scene).instantiate()
-		stageRoot.add_child(instantiated)
+		currentStatics.add_child(instantiated)
 	
 	var num:int = 0
-	for object in currentStage.modes.get(mode).map_zones[0].objects:
+	for object in currentStage.modes.get(currentMode).map_zones[currentArea].objects:
 		var instantiated:Node = RollableObject3D.instantiate()
 		
 		ResourceLoader.load_threaded_request(objectList.objects.get(object.id).view_mesh)
@@ -80,7 +111,7 @@ func instantiateStage(mode:String, parent:Node = get_tree().get_root()):
 		while ResourceLoader.load_threaded_get_status(objectList.objects.get(object.id).collision_mesh) == ResourceLoader.THREAD_LOAD_IN_PROGRESS:
 			await get_tree().create_timer(0.1).timeout
 		
-		instantiated.InstanceName = str(num)
+		instantiated.InstanceName = str(currentArea) + "_" + str(num)
 		instantiated.ObjectName = objectList.objects.get(object.id).name.en
 		instantiated.ObjectMesh = ResourceLoader.load_threaded_get(objectList.objects.get(object.id).view_mesh)
 		instantiated.ObjectCol = ResourceLoader.load_threaded_get(objectList.objects.get(object.id).collision_mesh)
@@ -92,22 +123,8 @@ func instantiateStage(mode:String, parent:Node = get_tree().get_root()):
 		
 		num += 1
 	
-	var katamari:Node = KatamariControllable.instantiate()
-	katamari.Size = currentStage.modes.get(mode).katamari.size
-	katamari.CoreModel = currentStage.modes.get(mode).katamari.model
-	katamari.CoreTexture = currentStage.modes.get(mode).katamari.texture
-	var positions:Array = currentStage.modes.get(mode).map_zones[0].spawn_positions
-	var randomPos = randi_range(0, positions.size() - 1)
-	katamari.position = Vector3(positions[randomPos][0], positions[randomPos][1], positions[randomPos][2])
-	katamari.Speed = currentStage.modes.get(mode).katamari.speed
-	katamari.CanDash = currentStage.modes.get(mode).katamari.can_dash
-	katamari.CanQuickTurn = currentStage.modes.get(mode).katamari.can_turn
-	katamari.CameraRotation = currentStage.modes.get(mode).katamari.cam_rotation
-	
-	katamari.CameraZones = currentStage.modes.get(mode).cam_zones
-	stageRoot.add_child(katamari)
-	
-	# INSTANTIATE TIMER HERE
-	
-	parent.add_child(stageRoot)
-	stage_instantiated.emit()
+	currentKatamari.RoyalWarpHeight = currentStage.modes.get(currentMode).map_zones[currentArea].warp_height
+	currentKatamari.SpawnPoints = currentStage.modes.get(currentMode).map_zones[currentArea].spawn_positions.map(func(pos): return Vector3(pos[0], pos[1], pos[2]))
+	currentKatamari.SoundSize = currentStage.modes.get(currentMode).map_zones[currentArea].audio_size
+	currentKatamari.CoreModel = currentStage.modes.get(currentMode).map_zones[currentArea].core_model
+	currentKatamari.CoreTexture = currentStage.modes.get(currentMode).map_zones[currentArea].core_texture
