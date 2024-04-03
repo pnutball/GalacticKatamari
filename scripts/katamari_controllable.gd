c481a4de9707ecb5c3a05ad584f7cd4cdcec1bfa
Ronan Banton <crealol2@outlook.com>
2024-04-03 11:04:48 -0400

    Cousin now rotates when rolling at an angle

--- scripts/katamari_controllable.gd
+++ scripts/katamari_controllable.gd
@@ -148,6 +148,7 @@ func _process(delta):
 	%OujiAnimTree.set("parameters/WalkState/Ouji_Walk/conditions/forward_condition", (StickMidpoint.angle_to(Vector2.UP) < (2.0*PI)/6.0) and (StickMidpoint.angle_to(Vector2.UP) > (-2.0*PI)/6.0))
 	%OujiAnimTree.set("parameters/WalkState/Ouji_Walk/conditions/backward_condition", (StickMidpoint.angle_to(Vector2.UP) > (4.0*PI)/6.0) or (StickMidpoint.angle_to(Vector2.UP) < (-4.0*PI)/6.0))
 	%OujiAnimTree.set("parameters/WalkState/Ouji_Walk/conditions/side_condition", (StickMidpoint.angle_to(Vector2.UP) > (2.0*PI)/6.0 and StickMidpoint.angle_to(Vector2.UP) < (4.0*PI)/6.0) or (StickMidpoint.angle_to(Vector2.UP) > (-4.0*PI)/6.0 and StickMidpoint.angle_to(Vector2.UP) < (-2.0*PI)/6.0))
+	%OujiAnimTree.set("parameters/WalkState/Ouji_Walk/Forward/OujiForwardAdd/add_amount", lerpf(%OujiAnimTree.get("parameters/WalkState/Ouji_Walk/Forward/OujiForwardAdd/add_amount"), StickAngle, 0.5))
 	%OujiAnimTree.set("parameters/MovementScale/scale", Speed / 4.5)
 	
 	# Update debug info
