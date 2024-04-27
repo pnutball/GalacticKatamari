705aebc824917990bd68937300c8c4adf00c0d5d
Ronan Banton <crealol2@outlook.com>
2024-04-27 01:26:43 -0400

    Did a bit of prep for an editor 3D view

---
+++ editor/3dview_script.gd
@@ -0,0 +1,10 @@
+extends SubViewportContainer
+
+func reload_statics(statics_dict:Dictionary):
+	pass
+
+func reload_objects(objects_dict:Dictionary):
+	pass
+
+func reload_spawns(spawns_dict:Dictionary):
+	pass
