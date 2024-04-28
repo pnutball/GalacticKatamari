d21fc35437707979d87c32b5bd35028afde38f0c
Ronan Banton <crealol2@outlook.com>
2024-04-27 22:25:52 -0400

    Added a bit more to editor's 3d view

--- editor/3dview_script.gd
+++ editor/3dview_script.gd
@@ -1,10 +1,16 @@
 extends SubViewportContainer
 
-func reload_statics(statics_dict:Dictionary):
-	pass
+func reload_statics():
+	#$EditorViewport/EditorPrevRoot/statics
+	for stat in %SplitLeft.lastSelectedArea.get_meta(&"path").static:
+		print(stat)
 
-func reload_objects(objects_dict:Dictionary):
-	pass
+func reload_objects():
+	#$EditorViewport/EditorPrevRoot/objects
+	for obj in %SplitLeft.lastSelectedArea.get_meta(&"path").objects:
+		print(obj)
 
-func reload_spawns(spawns_dict:Dictionary):
-	pass
+func reload_spawns():
+	#$EditorViewport/EditorPrevRoot/spawns
+	for spawn in %SplitLeft.lastSelectedArea.get_meta(&"path").spawn_positions:
+		print(spawn)
