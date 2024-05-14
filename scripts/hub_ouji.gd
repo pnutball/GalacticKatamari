fabc80fb49dd0d1ef994ac6268012c6e2e9cf057
Ronan Banton <crealol2@outlook.com>
2024-05-13 22:48:50 -0400

    Added basic hub area
    Area contains only a test rectangle and basic movement

---
+++ scripts/hub_ouji.gd
@@ -0,0 +1,27 @@
+extends CharacterBody3D
+## The input vector for the left stick (movement).
+var LeftStick:Vector2 = Vector2(0, 0)
+## The input vector for the right stick (camera).
+var RightStick:Vector2 = Vector2(0, 0)
+## The camera's X and Y rotations.
+## X range: -35deg to 90deg
+var CamRotation:Vector2 = Vector2(0, 5)
+## Movement speed.
+const Speed:float = 240
+
+func _physics_process(delta):
+	LeftStick = Input.get_vector("LS Right", "LS Left", "LS Down", "LS Up", 0.5).rotated(deg_to_rad(-1 * CamRotation.x))
+	RightStick = Input.get_vector("RS Right", "RS Left", "RS Down", "RS Up", 0.5)
+	velocity = Vector3(LeftStick.x, 0, LeftStick.y) * Speed * delta
+	if not is_zero_approx(Vector2(velocity.x, velocity.z).length()): 
+		$Ouji.rotation.y = lerp_angle($Ouji.rotation.y,
+		Vector2(velocity.z, velocity.x).angle(),
+		(30)*delta)
+	move_and_slide()
+
+func _process(delta):
+	CamRotation += 80 * RightStick * delta #* Vector2(1,-1)
+	CamRotation.y = clampf(CamRotation.y, -35, 90)
+	$HubOujiCamPivot.rotation = lerp($HubOujiCamPivot.rotation, 
+	Vector3(deg_to_rad(CamRotation.y), deg_to_rad(CamRotation.x), 0),
+	(15)*delta)
