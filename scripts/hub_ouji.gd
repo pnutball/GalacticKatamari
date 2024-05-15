66df51e17d94272f5f6a1d716eff155e37cc44fa
Ronan Banton <crealol2@outlook.com>
2024-05-14 23:28:53 -0400

    Run implemented

--- scripts/hub_ouji.gd
+++ scripts/hub_ouji.gd
@@ -8,13 +8,19 @@ var RightStick:Vector2 = Vector2(0, 0)
 var CamRotation:Vector2 = Vector2(0, 5)
 ## Mouse velocity.
 var MouseVelocity:Vector2 = Vector2.ZERO
-## Movement speed.
-var Speed = 0
+## Base movement speed.
+const Speed:float = 240
 
 func _physics_process(delta):
 	LeftStick = Input.get_vector("LS Right", "LS Left", "LS Down", "LS Up", 0.5).rotated(deg_to_rad(-1 * CamRotation.x))
 	RightStick = Input.get_vector("RS Right", "RS Left", "RS Down", "RS Up", 0.5)
-	velocity = Vector3(LeftStick.x, 0, LeftStick.y) * Speed * delta
+	velocity = Vector3(LeftStick.x, 0, LeftStick.y) * Speed
+	if Input.is_action_pressed("Hub Run"): 
+		velocity *= 2
+	var VelLength:float = velocity.length()
+	velocity *= delta
+	$HubOujiAnim.set("parameters/Move/Move/blend_amount", lerpf($HubOujiAnim.get("parameters/Move/Move/blend_amount"), clampf(VelLength - Speed, 0, Speed) / Speed, 15*delta))
+	$HubOujiAnim.set("parameters/Move/MoveSpeed/scale", VelLength / Speed)
 	if not is_zero_approx(Vector2(velocity.x, velocity.z).length()): 
 		$Ouji.rotation.y = lerp_angle($Ouji.rotation.y,
 		Vector2(velocity.z, velocity.x).angle(),
@@ -30,14 +36,8 @@ func _input(event):
 			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	elif event is InputEventMouseMotion:
 		MouseVelocity = event.relative
-	## Issue : Every time the walk cycle animation finishes, the speed is set back to 240. It works! It's just that odd issue. - puff
-	if event.is_action_pressed("Shift or Square Button"):
-		Speed = 400
-	else:
-		Speed = 240
 
 func _process(delta):
-	
 	CamRotation += 80 * RightStick * delta #* Vector2(1,-1)
 	if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
 		CamRotation += 8 * MouseVelocity * delta * Vector2(-1, 1)
