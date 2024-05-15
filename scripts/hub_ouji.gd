04f6afb8d3f8464245c2173ec80afdf89dd8d22e
puff2460 <audiopowell@gmail.com>
2024-05-14 20:49:18 -0500

    Added WIP running mechanic for Hub

--- scripts/hub_ouji.gd
+++ scripts/hub_ouji.gd
@@ -9,7 +9,7 @@ var CamRotation:Vector2 = Vector2(0, 5)
 ## Mouse velocity.
 var MouseVelocity:Vector2 = Vector2.ZERO
 ## Movement speed.
-const Speed:float = 240
+var Speed = 0
 
 func _physics_process(delta):
 	LeftStick = Input.get_vector("LS Right", "LS Left", "LS Down", "LS Up", 0.5).rotated(deg_to_rad(-1 * CamRotation.x))
@@ -30,9 +30,14 @@ func _input(event):
 			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	elif event is InputEventMouseMotion:
 		MouseVelocity = event.relative
-	
+	## Issue : Every time the walk cycle animation finishes, the speed is set back to 240. It works! It's just that odd issue. - puff
+	if event.is_action_pressed("Shift or Square Button"):
+		Speed = 400
+	else:
+		Speed = 240
 
 func _process(delta):
+	
 	CamRotation += 80 * RightStick * delta #* Vector2(1,-1)
 	if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
 		CamRotation += 8 * MouseVelocity * delta * Vector2(-1, 1)
