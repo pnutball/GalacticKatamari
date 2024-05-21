061de2a5fd42bc1226fe37f502b758589abe26f7
Ronan Banton <crealol2@outlook.com>
2024-05-21 16:34:56 -0400

    Added logo screen

---
+++ scripts/logo_screen.gd
@@ -0,0 +1,12 @@
+extends Control
+
+func _ready():
+	# The Prince fly down animation a-la BK goes here at some point
+	# TODO: Add ability to skip by pressing A/Cross/Start/Enter/Space
+	$LogoAnimation.animation_set_next(&"DevBy", &"PowBy")
+	$LogoAnimation.animation_set_next(&"PowBy", &"Fade")
+	$LogoAnimation.animation_set_next(&"Fade", &"LoadMenu")
+	$LogoAnimation.play(&"DevBy")
+
+func LogosDone():
+	get_tree().change_scene_to_file("res://scenes/debug_menu.tscn")
