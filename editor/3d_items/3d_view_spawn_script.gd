823c2b7310433c9c987b4d531c8452aabc470feb
Ronan Banton <crealol2@outlook.com>
2024-06-26 02:11:13 -0400

    Did a bit of work on editor gizmos

--- editor/3d_items/3d_view_spawn_script.gd
+++ editor/3d_items/3d_view_spawn_script.gd
@@ -3,10 +3,15 @@ extends Node3D
 ## The source tree object used for this 3D spawn.
 @export var Source:GKEditorTreeSpawn = null
 
+var dragging:bool = false
+
 func _process(_delta):
-	if Source != null:
+	if Source != null and not dragging:
 		position = Source.position
-		rotation.y = deg_to_rad(Source.rotation)
+		$Visual.rotation.y = deg_to_rad(Source.rotation) - (PI*0.5)
+	else:
+		Source.position = position
+		Source.rotation = rad_to_deg($Visual.rotation.y) + 90
 
 func _dragged():
 	while Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
