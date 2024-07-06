b9ed070b09d6a3b27404de6494b648381a809765
Ronan Banton <crealol2@outlook.com>
2024-07-05 23:26:36 -0400

    Katamari image is now rendered during the Royal Rainbow

--- scripts/katamari_controllable.gd
+++ scripts/katamari_controllable.gd
@@ -185,11 +185,12 @@ func _end_stage():
 		DialogueBox.queue_dialog_string(GKGlobal.get_localized_string(mode.get("win_dialogue", {})), DialogueBox.MODE_IN)
 		DialogueBox.speak_queue()
 		await DialogueBox.queue_finished
-		capture_katamari_image()
 		if has_node("Timer"): $Timer.scroll_out()
 		$SizeMeter/SizeAnimation.play("Disappear")
 		await $SizeMeter/SizeAnimation.animation_finished
 		KingFace.get_node("RoyalRainbowAnimation").play("rainbow")
+		await get_tree().create_timer(4).timeout
+		capture_katamari_image()
 		await KingFace.get_node("MoyaInOutAnimation").animation_finished
 		await get_tree().create_timer(0.5).timeout
 		get_tree().change_scene_to_file("res://scenes/debug_menu.tscn")
@@ -198,20 +199,16 @@ func _end_stage():
 		DialogueBox.queue_dialog_string(GKGlobal.get_localized_string(mode.get("lose_dialogue", {})), DialogueBox.MODE_IN)
 
 func capture_katamari_image() -> void:
-	%ViewportRect.texture = ImageTexture.create_from_image($SubViewport.get_texture().get_image())
 	%KatamariCamera.cull_mask = 1
 	%KatamariCamera.attributes.set("dof_blur_far_enabled", false)
 	%KatamariCamera.environment = preload("res://scenes/environment/env_preview_env.tres")
 	$SubViewport.transparent_bg = true
 	await RenderingServer.frame_post_draw
-	RenderingServer.force_draw()
 	GKGlobal.create_result_katamari_image($SubViewport.get_texture().get_image())
 	$SubViewport.transparent_bg = false
 	%KatamariCamera.environment = null
 	%KatamariCamera.attributes.set("dof_blur_far_enabled", true)
 	%KatamariCamera.cull_mask = 786431
-	RenderingServer.force_draw()
-	%ViewportRect.texture = $SubViewport.get_texture()
 
 func _process(delta):
 	$SubViewport.size = %ViewportRect.get_viewport_rect().size
