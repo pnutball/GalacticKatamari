d438275662b8833ad8da306a887f5217657121b2
Ronan Banton <crealol2@outlook.com>
2024-07-19 23:40:52 -0400

    Ouji view no longer uses own world
    This allows the ouji model to appear behind the katamari without animating a copy.

--- scripts/ouji_view.gd
+++ scripts/ouji_view.gd
@@ -5,12 +5,19 @@ var FatiguePrev:float = 1
 func _ready():
 	%OujiAnimTree.active = true
 	var OujiModel = load(GKGlobal.OujiInfo[GKGlobal.players[0][0]].get("Model")).instantiate()
-	OujiModel.position = Vector3(0.05, -0.835, -1.51)
+	OujiModel.position = Vector3.ZERO
 	OujiModel.scale = Vector3(0.038, 0.038, 0.038)
-	$OujiViewportContainer/SubViewport.add_child(OujiModel)
+	$OujiViewportContainer/SubViewport/OujiViewRoot.add_child(OujiModel)
 	%OujiAnimTree.anim_player = ^"../Ouji/OujiAnimation"
 
 func _process(delta):
+	$OujiViewportContainer/SubViewport/OujiViewRoot.global_position = (
+		Vector3($"../KatamariBody".global_position.x, $"../FloorBumpDetect".global_position.y, $"../KatamariBody".global_position.z)
+		 + (Vector3(0, 0, 0.35 * $"..".Size + 0.02).rotated(Vector3.UP, $"../SubViewport/KatamariCameraPivot".global_rotation.y)) * StageLoader.stageRoot.scale
+	)
+	$OujiViewportContainer/SubViewport/OujiViewRoot.global_rotation.y = $"../SubViewport/KatamariCameraPivot".global_rotation.y - PI
+	$OujiViewportContainer/SubViewport/OujiViewRoot.scale = StageLoader.stageRoot.scale * 1.31 * 0.05
+	
 	# Animate cousin model
 	var rotatedVelocity = Vector2($"../KatamariBody".linear_velocity.x, $"../KatamariBody".linear_velocity.z).rotated($"..".CameraRotation)
 	%OujiAnimTree.set("parameters/WalkState/conditions/idle_speed_reached", $"../KatamariBody".linear_velocity.length() < ($"..".Size / 10))
