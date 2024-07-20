c815649dc9424b3af22d708c6fd21fabbbd3799d
Ronan Banton <crealol2@outlook.com>
2024-07-19 23:41:32 -0400

    Controls viewer now can be closed in demo title

--- scripts/controls_viewer.gd
+++ scripts/controls_viewer.gd
@@ -1,11 +1,15 @@
 extends Control
 
 @export var TextShown = true
+const INFO_TEXT:String = "This game is played with both %s.\n(If you don't know this, why are you\neven doing a Katamari month?)"
 
 func _ready():
 	GKGlobal.controller_changed.connect(_refresh_image)
 	$HBoxContainer/Middle/VBoxContainer/InfoLabel.visible = TextShown
 	_refresh_image()
+
+func _process(_delta):
+	$HBoxContainer/Middle/VBoxContainer/InfoLabel.text = INFO_TEXT % ("sticks" if GKGlobal.usingController else "WASD and IJKL")
 
 func _refresh_image(device:int = 0):
 	if device == 0: 
--- scripts/title_screen.gd
+++ scripts/title_screen.gd
@@ -2,6 +2,8 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	_refresh_images()
+	GKGlobal.controller_changed.connect(func(index:int): if index == 0: _refresh_images())
 	$Ouji/OujiAnimation.play("Ouji_Idle")
 	$Title/FadeOverlay/FadeOverlayAnim.play("FadeIn")
 	await $Title/FadeOverlay/FadeOverlayAnim.animation_finished
@@ -9,15 +11,25 @@ func _ready():
 	$Title/CopyrightText/CopyrightFade.play("fade_in")
 	$Title/TitleLogo/LogoAnimation.play("ShowLogo")
 	await $Title/TitleLogo/LogoAnimation.animation_finished
-	$Title/StartText.visible = true
-	$Title/StartText/StartFadeAnim.play("FadeInOut")
-	
+	$Title/Start.visible = true
+	$Title/Start/StartFadeAnim.play("FadeInOut")
+
+var start_key:Texture2D = GKGlobal.get_key(KEY_ENTER)
+var start_button:Texture2D
+
+func _process(_delta):
+	if has_node("Title/Start/StartImg"):
+		$Title/Start/StartImg.texture = start_button if GKGlobal.usingController else start_key
+
+func _refresh_images():
+	start_button = GKGlobal.get_button(GKGlobal.BUTTON_START, false, GKGlobal.get_brand())
 
 func _input(_event):
 	if Input.is_action_just_pressed("Confirm") and $Controls.visible:
-		pass
-	elif Input.is_action_just_pressed("Confirm") and has_node("Title/StartText") and $Title/StartText.visible:
-		$Title/StartText.visible = false
+		$Controls/ControlsFadeAnim.play("fade_out")
+		await $Controls/ControlsFadeAnim.animation_finished
+	elif (Input.is_action_just_pressed("Confirm") or Input.is_action_just_pressed("Pause")) and has_node("Title/Start") and $Title/Start.visible:
+		$Title/Start.visible = false
 		$ConfirmSound.play()
 		if not GKGlobal.is_demo:
 			$Title/FadeOverlay/FadeOverlayAnim.play("FadeOut")
